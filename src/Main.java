9684891e69443a8d36495492562f2900dd9185bb
Maria <mariatur776@gmail.com>
2023-03-08 13:14:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13_676;
+
+        int bonus = price / 20;
+        System.out.println("Бонус :" + bonus);
+
+    }
+}
